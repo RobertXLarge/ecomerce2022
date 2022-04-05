4ec27c858b786d70ee10a068fd2225d058504cb2
RobertXLarge <elvivipin01@hotmail.com>
2022-04-05 10:45:18 -0400

    1 file modified, 4 added, 3 deleted

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Header from './componentes/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Encabezado</div>
+  )
+}
+
+export default Header
