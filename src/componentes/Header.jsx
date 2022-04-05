aa0090d27e3dd92dc0ebaaca312c6d5e91c7632c
RobertXLarge <elvivipin01@hotmail.com>
2022-04-05 14:30:26 -0400

    1 file modified

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   const displayMobile = () =>  {}
   const displayDesktop = () =>  {
     return(<Toolbar className={ classes.toolbar}>
-      <img src={logo}/>
+      <img src={logo} className={classes.logo}/>
       </Toolbar>)
   }
   
@@ -24,7 +24,19 @@ const Header = () => {
 };
 
 const useStyle = makeStyles ((theme)=>({
-  toolbar: {}
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    allignItems: "center",
+
+  },
+
+  logo: {
+    height: "30px",
+    margin: theme.spacing(1,0,0,2),
+    objectFit: "contain",
+  }
+
 }))
 
 export default Header;
